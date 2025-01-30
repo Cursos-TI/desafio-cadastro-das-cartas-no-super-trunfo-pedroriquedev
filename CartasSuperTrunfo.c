ddaf5c4051f865215b4ccfb008c002a2ae196543
pedroriquedev <phh.paulino@gmail.com>
2025-01-30 22:17:30 +0000

    cadastros de todas as cartas concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,24 +8,658 @@ int main(){
     int pontos_turisticos;
 
     printf("Bem-vindo ao cadastro de cartas do jogo Super Trunfo! \n");
-    printf("Insira os dados para a carta (A01) \n");
+    printf("\n");
+    printf("Insira os dados das cartas para o primeiro estado. \n");
+    printf("CARTA A01 \n");
 
-    printf("digite a populacao: ");
+    printf("Populacao: ");
     scanf("%d", &populacao);
 
-    printf("Digite a area da cidade: ");
+    printf("Area (em km²): ");
     scanf("%f", &area);
 
-    printf("Digite o pib da cidade:" );
+    printf("Pib: ");
     scanf("%f", &pib);
 
-    printf("Digite o numero de pontos turisticos: ");
+    printf("Numero de pontos turisticos: ");
     scanf("%d", &pontos_turisticos);
+    printf("\n");
 
-    printf("Populacao: %d\nArea: %f\n", populacao, area);
-    printf("Pib: %f\npontos Turisticos: %d\n", pib, pontos_turisticos);
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
 
+     printf("\n----------------------------------------------------------------------------\n");
 
+    printf("CARTA A02 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA A03 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA A04 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("Insira os dados das cartas para o segundo estado. \n");
+    printf("CARTA B01: \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA B02 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA B03 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA B04 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("Insira os dados das cartas para o terceiro estado. \n");
+    printf("CARTA C01 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+    
+    printf("CARTA C02 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+    
+    printf("CARTA C03 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+    
+    printf("CARTA C04 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("Insira os dados das cartas para o quarto estado. \n");
+    printf("CARTA D01 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA D02 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA D03 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA D04 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("Insira os dados das cartas para o quinto estado. \n");
+    printf("CARTA E01 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA E02 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA E03 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA E04 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("Insira os dados das cartas para o sexto estado. \n");
+    printf("CARTA F01 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA F02 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA F03 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA F04 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("Insira os dados das cartas para o setimo estado. \n");
+    printf("CARTA G01 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA G02 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA G03 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA G04 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("Insira os dados das cartas para o oitavo estado. \n");
+    printf("CARTA H01 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA H02 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA H03 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+     printf("\n----------------------------------------------------------------------------\n");
+
+    printf("CARTA H04 \n");
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &area);
+
+    printf("Pib: ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    printf("Populacao: %d - Area: %f", populacao, area);
+    printf(" - Pib: %f - pontos Turisticos: %d", pib, pontos_turisticos);
+
+    printf("\n----------------------------------------------------------------------------\n");
+
+    printf("VOCE ACABOU DE CADASTRAR TODAS AS CARTAS DO JOGO!");
+    printf("\n");
+    
     return 0;
     
 }
